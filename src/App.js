9df48edec419a7c8da491d9490fa62d6d0ad41fa
bhuvan malik <bhuvanmalik007@gmail.com>
2020-07-08 02:55:54 -0400

    Initialised CRA with Material-UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import HideOnScroll from "./components/HideOnScroll";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(({ mixins, spacing, transitions, zIndex }) => ({
+  root: {
+    display: "flex",
+    height: "100%",
+  },
+  appBar: {
+    transition: transitions.create(['margin', 'width'], {
+      easing: transitions.easing.easeInOut,
+      duration: transitions.duration.leavingScreen,
+    }),
+    // zIndex: zIndex.drawer + 1,
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <HideOnScroll>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              S&P 500 Total Returns
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Box display="flex" justifyContent="flex-start" p={1} bgcolor="primary">
+        {"I'm a flexbox container!"}
+      </Box>
     </div>
   );
 }
